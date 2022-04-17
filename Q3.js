2f584fcb946205b3288d3a50bea421377e9fc091
HggLpp <100394770+HggLpp@users.noreply.github.com>
2022-04-17 16:26:21 +0800

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,29 @@
+function enterAnswer(){
+  var response = prompt();
+  if (response === "Love"){
+    alert("Excellent!")
+    alert("Congradulations!You finish all the puzzles!")
+  }
+  else if (response === "love"){
+    alert("Great!")
+    alert("Congradulations!You finish all the puzzles!")
+  }
+  else{
+    alert("Are you sure?")
+    alert("Wrong, think about it again.")
+  }
+}
+
+function getAnswer(){
+  var x = document.getElementById("answer3");
+  x.innerHTML = "Love";
+  x.style.fontSize = "35px";
+  x.style.color = "green";
+}
+
+function getTips(){
+var tips = ["This is a beautiful thing.","It will make you happy."];
+for(i=0;i<3;i++){
+  alert(tips[i]);
+}
+}
